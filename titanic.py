c9a77423898b483e67df195ef2bb51607848916b
LyangHiga <lyang_higa@poli.ufrj.br>
2019-06-04 13:27:12 -0300

    refactoring

--- titanic.py
+++ titanic.py
@@ -20,6 +20,10 @@ from sklearn.tree import DecisionTreeClassifier
 
 from sklearn.model_selection import cross_val_score
 
+#TODO refactoring
+#TODO pipeline
+#TODO Regularization
+
 
 def plot_hist(titanic):
 	titanic.hist(bins=50, figsize=(20,15))
@@ -33,6 +37,109 @@ def plot_hist_feat_surv(feature):
 	plt.savefig(feature)
 	plt.show()
 
+def data_transformation(dset):
+	#convert male to 0 and female to 1
+	for dataset in dset:
+	    dataset['Sex'] = dataset['Sex'].map( {'female': 1, 'male': 0} ).astype(int)
+
+	#for Embarked there are two missing values, we'll put the most common
+	for dataset in dset:
+		freq_port = dataset.Embarked.dropna().mode()[0]	
+	for dataset in dset:
+		dataset['Embarked'] = dataset['Embarked'].fillna(freq_port)
+		dataset['Embarked'] = dataset['Embarked'].map( {'S': 0, 'C': 1, 'Q': 2} ).astype(int)
+
+	#For age we also have missing values
+	#for now lets use the median
+	for dataset in dset:
+		dataset["Age"] = dataset["Age"].fillna(dataset.loc[:,"Age"].median())
+	for dataset in dset:
+		dataset["Fare"] = dataset["Fare"].fillna(dataset.loc[:,"Fare"].median())	
+
+	#add a new feature Title
+	for dataset in dset:
+		dataset['Title'] = dataset.Name.str.extract(' ([A-Za-z]+)\.', expand=False)
+
+
+	title_mapping = {"Mr": 1, "Miss": 2, "Mrs": 3, "Master": 4, "Rare": 5}
+
+	for dataset in dset:
+	    dataset['Title'] = dataset['Title'].replace(['Lady', 'Countess','Capt', 'Col',\
+	 	'Don', 'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer', 'Dona'], 'Rare')
+
+	    dataset['Title'] = dataset['Title'].replace('Mlle', 'Miss')
+	    dataset['Title'] = dataset['Title'].replace('Ms', 'Miss')
+	    dataset['Title'] = dataset['Title'].replace('Mme', 'Mrs')
+
+	    dataset['Title'] = dataset['Title'].map(title_mapping)
+	    dataset['Title'] = dataset['Title'].fillna(0)
+
+def del_feat(dset):
+	dset = dset.drop("Name",axis=1)
+	dset = dset.drop("Ticket",axis=1)
+	dset = dset.drop("Cabin",axis=1)
+	dset = dset.drop("PassengerId",axis=1)
+	return dset
+
+def correlations():
+	corr_matrix = train.corr()
+	print(corr_matrix["Survived"].sort_values(ascending=False))
+	#we can also get feature correlations by pivoting categorical features with our label
+	print(train[["Pclass","Survived"]].groupby(["Pclass"],as_index=False).mean().sort_values(by="Survived", ascending=False))
+	print(train[["Sex","Survived"]].groupby(["Sex"],as_index=False).mean().sort_values(by="Survived", ascending=False))
+	print(train[["SibSp","Survived"]].groupby(["SibSp"],as_index=False).mean().sort_values(by="Survived", ascending=False))
+	print(train[["Parch","Survived"]].groupby(["Parch"],as_index=False).mean().sort_values(by="Survived", ascending=False))
+	print(train[["Embarked","Survived"]].groupby(["Embarked"],as_index=False).mean().sort_values(by="Survived", ascending=False))
+	print(train[['Title', 'Survived']].groupby(['Title'], as_index=False).mean())
+
+	plot_hist_feat_surv("Age")
+	plot_hist_feat_surv("Fare")
+	plot_hist_feat_surv("Pclass")
+	plot_hist_feat_surv("Sex")
+	plot_hist_feat_surv("Embarked")
+	plot_hist_feat_surv("Title")
+
+def acc_score(clf,name,x,y):
+	acc = round(clf.score(x, y) * 100, 2)
+	print(name + " Accuracy : " + str(acc))
+
+def feat_scaling(dset,l_feat):
+	scaler = StandardScaler()
+	for feat in l_feat:
+		dset[feat] = scaler.fit_transform(dset[feat].values.reshape(-1,1))
+	return dset
+
+def bands(dset):
+	train['AgeBand'] = pd.cut(train['Age'], 5)
+	#print(train[['AgeBand', 'Survived']].groupby(['AgeBand'], as_index=False).mean().sort_values(by='AgeBand', ascending=True))
+	
+	dset.loc[ dset['Age'] <= 16, 'Age'] = 0
+	dset.loc[(dset['Age'] > 16) & (dset['Age'] <= 32), 'Age'] = 1
+	dset.loc[(dset['Age'] > 32) & (dset['Age'] <= 48), 'Age'] = 2
+	dset.loc[(dset['Age'] > 48) & (dset['Age'] <= 64), 'Age'] = 3
+	dset.loc[ dset['Age'] > 64, 'Age']
+
+	train['FareBand'] = pd.qcut(train['Fare'], 4)
+	#print(train[['FareBand', 'Survived']].groupby(['FareBand'], as_index=False).mean().sort_values(by='FareBand', ascending=True))
+
+	dset.loc[ dset['Fare'] <= 7.91, 'Fare'] = 0
+	dset.loc[(dset['Fare'] > 7.91) & (dset['Fare'] <= 14.454), 'Fare'] = 1
+	dset.loc[(dset['Fare'] > 14.454) & (dset['Fare'] <= 31), 'Fare']   = 2
+	dset.loc[ dset['Fare'] > 31, 'Fare'] = 3
+	dset['Fare'] = dset['Fare'].astype(int)
+
+	return dset
+
+def sub(clf):
+	Y_pred = clf.predict(test)
+
+	submission = pd.DataFrame({
+	        "PassengerId": X_test["PassengerId"],
+	        "Survived": Y_pred
+	    })
+
+	submission.to_csv('../Titanic-Machine-Learning-from-Disaster/submission.csv', index=False)
+
 
 #just import train and test set 
 train = pd.read_csv("train.csv")
@@ -40,39 +147,7 @@ test = pd.read_csv("test.csv")
 
 X_test = test.copy()
 
-#convert male to 0 and female to 1
-for dataset in train, test:
-    dataset['Sex'] = dataset['Sex'].map( {'female': 1, 'male': 0} ).astype(int)
-
-#for Embarked there are two missing values, we'll put the most common
-freq_port = train.Embarked.dropna().mode()[0]	
-for dataset in train, test:
-	dataset['Embarked'] = dataset['Embarked'].fillna(freq_port)
-	dataset['Embarked'] = dataset['Embarked'].map( {'S': 0, 'C': 1, 'Q': 2} ).astype(int)
-
-#For age we also have missing values
-#for now lets use the median
-for dataset in train, test:
-	dataset["Age"] = dataset["Age"].fillna(dataset.loc[:,"Age"].median())
-
-#add a new feature Title
-for dataset in train, test:
-	dataset['Title'] = dataset.Name.str.extract(' ([A-Za-z]+)\.', expand=False)
-
-
-title_mapping = {"Mr": 1, "Miss": 2, "Mrs": 3, "Master": 4, "Rare": 5}
-
-for dataset in train, test:
-    dataset['Title'] = dataset['Title'].replace(['Lady', 'Countess','Capt', 'Col',\
- 	'Don', 'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer', 'Dona'], 'Rare')
-
-    dataset['Title'] = dataset['Title'].replace('Mlle', 'Miss')
-    dataset['Title'] = dataset['Title'].replace('Ms', 'Miss')
-    dataset['Title'] = dataset['Title'].replace('Mme', 'Mrs')
-
-    dataset['Title'] = dataset['Title'].map(title_mapping)
-    dataset['Title'] = dataset['Title'].fillna(0)
-
+data_transformation([train, test])
 
 #separe features from labels
 titanic = train.copy()
@@ -80,40 +155,17 @@ titanic = titanic.drop("Survived", axis=1)
 titanic_labels = train["Survived"].copy()
 #print(titanic.head())
 #print(titanic_labels.head())
-print(titanic.info())
-print(titanic.describe())
-
+#print(titanic.info())
+#print(titanic.describe())
 #plot_hist(titanic)
 
-
-#for now we'll ignore name, ticket and cabin
-titanic = titanic.drop("Name",axis=1)
-titanic = titanic.drop("Ticket",axis=1)
-titanic = titanic.drop("Cabin",axis=1)
-titanic = titanic.drop("PassengerId",axis=1)
-print(titanic.info())
-#print(titanic.head())
+titanic = del_feat(titanic)
+#print(titanic.info())
+#print(titanic.describe())
 
 #lets look to correlations
-corr_matrix = train.corr()
-print(corr_matrix["Survived"].sort_values(ascending=False))
-#we can also get feature correlations by pivoting categorical features with our label
-print(train[["Pclass","Survived"]].groupby(["Pclass"],as_index=False).mean().sort_values(by="Survived", ascending=False))
-print(train[["Sex","Survived"]].groupby(["Sex"],as_index=False).mean().sort_values(by="Survived", ascending=False))
-print(train[["SibSp","Survived"]].groupby(["SibSp"],as_index=False).mean().sort_values(by="Survived", ascending=False))
-print(train[["Parch","Survived"]].groupby(["Parch"],as_index=False).mean().sort_values(by="Survived", ascending=False))
-print(train[["Embarked","Survived"]].groupby(["Embarked"],as_index=False).mean().sort_values(by="Survived", ascending=False))
-print(train[['Title', 'Survived']].groupby(['Title'], as_index=False).mean())
+#correlations()
 
-#plot_hist_feat_surv("Age")
-#plot_hist_feat_surv("Fare")
-#plot_hist_feat_surv("Pclass")
-#plot_hist_feat_surv("Sex")
-#plot_hist_feat_surv("Embarked")
-#plot_hist_feat_surv("Title")
-
-print(titanic.info())
-print(titanic.head())
 
 sgd_clf = SGDClassifier(loss = 'modified_huber',max_iter=5, tol=-np.infty, random_state=42)
 sgd_clf.fit(titanic, titanic_labels)
@@ -124,135 +176,80 @@ forest_clf.fit(titanic, titanic_labels)
 svc = SVC(probability=True)
 svc.fit(titanic, titanic_labels)
 
-acc_sgd = round(sgd_clf.score(titanic, titanic_labels) * 100, 2)
-acc_forest = round(forest_clf.score(titanic, titanic_labels) * 100, 2)
-acc_svc = round(svc.score(titanic, titanic_labels) * 100, 2)
+log_clf = LogisticRegression()
+log_clf.fit(titanic, titanic_labels)
 
-print("sgd Accuracy in train_set before scaler: ",acc_sgd)
-print("RF Accuracy in train_set before scaler: ",acc_forest)
-print("svc Accuracy in train_set before scaler: ",acc_svc)
+acc_score(sgd_clf, "sgd_clf before scaler",titanic,titanic_labels)
+acc_score(forest_clf,"RF before scaler",titanic,titanic_labels )
+acc_score(svc,"svc before scaler",titanic,titanic_labels )
+acc_score(log_clf,"log_clf before scaler",titanic,titanic_labels )
 
-
-print('sgd_clf Accuracy using CV with 10 folds: ', cross_val_score(sgd_clf, titanic, titanic_labels, cv=10, scoring="accuracy"))
-print('forest Accuracy using CV with 10 folds: ', cross_val_score(forest_clf, titanic, titanic_labels, cv=10, scoring="accuracy"))
+#print('sgd_clf Accuracy using CV with 10 folds: ', cross_val_score(sgd_clf, titanic, titanic_labels, cv=10, scoring="accuracy"))
+#print('forest Accuracy using CV with 10 folds: ', cross_val_score(forest_clf, titanic, titanic_labels, cv=10, scoring="accuracy"))
 
 #Feature Scaling
 #RN just Age ang Fare arent a categorical feature and Fare has a huge scaling 
 scaler = StandardScaler()
 titanic_scaled = titanic.copy()
-titanic_scaled["Age"] = scaler.fit_transform(titanic["Age"].values.reshape(-1,1))
-titanic_scaled["Fare"] = scaler.fit_transform(titanic["Fare"].values.reshape(-1,1))
-print(titanic_scaled.info())
-print(titanic_scaled.head())
+l_feat = ["Age","Fare"]
+titanic_scaled = feat_scaling(titanic_scaled,l_feat)
 
 sgd_clf.fit(titanic_scaled, titanic_labels)
 forest_clf.fit(titanic_scaled, titanic_labels)
 svc.fit(titanic_scaled, titanic_labels)
+log_clf.fit(titanic_scaled, titanic_labels)
 
-acc_sgd = round(sgd_clf.score(titanic, titanic_labels) * 100, 2)
-acc_forest = round(forest_clf.score(titanic, titanic_labels) * 100, 2)
-acc_svc = round(svc.score(titanic, titanic_labels) * 100, 2)
-print("sgd Accuracy in train_set after scaler: ",acc_sgd)
-print("RF Accuracy in train_set after scaler: ",acc_forest)
-print("svc Accuracy in train_set after scaler: ",acc_svc)
+acc_score(sgd_clf, "sgd_clf after scaler",titanic_scaled,titanic_labels)
+acc_score(forest_clf,"RF after scaler",titanic_scaled,titanic_labels )
+acc_score(svc,"svc after scaler",titanic_scaled,titanic_labels )
+acc_score(log_clf,"log_clf after scaler",titanic_scaled,titanic_labels )
 
-print('sgd_clf Accuracy using CV with 10 folds: ', cross_val_score(sgd_clf, titanic, titanic_labels, cv=10, scoring="accuracy"))
-print('forest Accuracy using CV with 10 folds: ', cross_val_score(forest_clf, titanic, titanic_labels, cv=10, scoring="accuracy"))
+#print('sgd_clf Accuracy using CV with 10 folds: ', cross_val_score(sgd_clf, titanic, titanic_labels, cv=10, scoring="accuracy"))
+#print('forest Accuracy using CV with 10 folds: ', cross_val_score(forest_clf, titanic, titanic_labels, cv=10, scoring="accuracy"))
 
 #instead of using Stander Scaler we can use Bands to categorize
 titanic_bands = titanic.copy()
 train['AgeBand'] = pd.cut(train['Age'], 5)
-print(train[['AgeBand', 'Survived']].groupby(['AgeBand'], as_index=False).mean().sort_values(by='AgeBand', ascending=True))
-
-titanic_bands.loc[ titanic_bands['Age'] <= 16, 'Age'] = 0
-titanic_bands.loc[(titanic_bands['Age'] > 16) & (titanic_bands['Age'] <= 32), 'Age'] = 1
-titanic_bands.loc[(titanic_bands['Age'] > 32) & (titanic_bands['Age'] <= 48), 'Age'] = 2
-titanic_bands.loc[(titanic_bands['Age'] > 48) & (titanic_bands['Age'] <= 64), 'Age'] = 3
-titanic_bands.loc[ titanic_bands['Age'] > 64, 'Age']
-
-train['FareBand'] = pd.qcut(train['Fare'], 4)
-print(train[['FareBand', 'Survived']].groupby(['FareBand'], as_index=False).mean().sort_values(by='FareBand', ascending=True))
-
-titanic_bands.loc[ titanic_bands['Fare'] <= 7.91, 'Fare'] = 0
-titanic_bands.loc[(titanic_bands['Fare'] > 7.91) & (titanic_bands['Fare'] <= 14.454), 'Fare'] = 1
-titanic_bands.loc[(titanic_bands['Fare'] > 14.454) & (titanic_bands['Fare'] <= 31), 'Fare']   = 2
-titanic_bands.loc[ titanic_bands['Fare'] > 31, 'Fare'] = 3
-titanic_bands['Fare'] = titanic_bands['Fare'].astype(int)
-
-print(titanic_bands.info())
-print(titanic_bands.head())
+titanic_bands = bands(titanic_bands)
 
 sgd_clf.fit(titanic_bands, titanic_labels)
 forest_clf.fit(titanic_bands, titanic_labels)
 svc.fit(titanic_bands, titanic_labels)
+log_clf.fit(titanic_bands, titanic_labels)
 
-
-acc_sgd = round(sgd_clf.score(titanic_bands, titanic_labels) * 100, 2)
-acc_forest = round(forest_clf.score(titanic_bands, titanic_labels) * 100, 2)
-acc_svc = round(svc.score(titanic_bands, titanic_labels) * 100, 2)
-print("sgd Accuracy in train_set bands: ",acc_sgd)
-print("RF Accuracy in train_set bands: ",acc_forest)
-print("svc Accuracy in train_set bands: ",acc_svc)
-
-log_clf = LogisticRegression()
+acc_score(sgd_clf, "sgd_clf bands",titanic_bands,titanic_labels)
+acc_score(forest_clf,"RF bands",titanic_bands,titanic_labels )
+acc_score(svc,"svc bands",titanic_bands,titanic_labels )
+acc_score(log_clf,"log_clf bands",titanic_bands,titanic_labels )
+'''
 
 bag_clf = BaggingClassifier(
-	DecisionTreeClassifier(), n_estimators=500,
+	RandomForestClassifier(n_estimators=100, random_state=42), n_estimators=500,
 	max_samples=100, bootstrap=True, n_jobs=-1
 )
 bag_clf.fit(titanic_bands, titanic_labels)
-acc_bag = round(bag_clf.score(titanic_bands, titanic_labels) * 100, 2)
-print("BaggingClassifier Accuracy in train_set bands",acc_bag)
+acc_score(bag_clf,"bag_clf bands",titanic_bands,titanic_labels )
+'''
 
 voting_clf_hard = VotingClassifier(
-	estimators = [('sgd',sgd_clf), ('svm',svc), ('forest',forest_clf), ('lr', log_clf) ],
+	estimators = [('svm',svc), ('forest',forest_clf)],
 	voting = 'hard'
 	)
-voting_clf_hard.fit(titanic_bands, titanic_labels)
-acc_vch = round(voting_clf_hard.score(titanic_bands, titanic_labels) * 100, 2)
+voting_clf_hard.fit(titanic, titanic_labels)
+acc_score(voting_clf_hard,"voting_clf_hard before scaler",titanic,titanic_labels )
 
 voting_clf_soft = VotingClassifier(
-	estimators = [('svm',svc), ('forest',forest_clf),('bag',bag_clf)],
+	estimators = [('svm',svc), ('forest',forest_clf)],
 	voting = 'soft'
 	)
-voting_clf_soft.fit(titanic_bands, titanic_labels)
-acc_vcs = round(voting_clf_soft.score(titanic_bands, titanic_labels) * 100, 2)
+voting_clf_soft.fit(titanic, titanic_labels)
+acc_score(voting_clf_soft,"voting_clf_soft before scaler",titanic,titanic_labels )
 
-print("voting_clf_hard Accuracy in train_set bands: ",acc_vch)
-print("voting_clf_soft Accuracy in train_set bands: ",acc_vcs)
-
-
-test = test.drop("Name",axis=1)
-test = test.drop("Ticket",axis=1)
-test = test.drop("Cabin",axis=1)
-test = test.drop("PassengerId",axis=1)
-#test["Age"] = scaler.fit_transform(test["Age"].values.reshape(-1,1))
-#test["Fare"] = scaler.fit_transform(test["Fare"].values.reshape(-1,1))
-
-test.loc[ test['Age'] <= 16, 'Age'] = 0
-test.loc[(test['Age'] > 16) & (test['Age'] <= 32), 'Age'] = 1
-test.loc[(test['Age'] > 32) & (test['Age'] <= 48), 'Age'] = 2
-test.loc[(test['Age'] > 48) & (test['Age'] <= 64), 'Age'] = 3
-test.loc[ test['Age'] > 64, 'Age']
-
-test.loc[ test['Fare'] <= 7.91, 'Fare'] = 0
-test.loc[(test['Fare'] > 7.91) & (test['Fare'] <= 14.454), 'Fare'] = 1
-test.loc[(test['Fare'] > 14.454) & (test['Fare'] <= 31), 'Fare']   = 2
-test.loc[ test['Fare'] > 31, 'Fare'] = 3
-
-test["Fare"] = test["Fare"].fillna(train.loc[:,"Fare"].median())	
+test = del_feat(test)
+#test = feat_scaling(test,l_feat)
+test = bands(test)
 
 print(test.info())
 print(test.head())
 
-Y_pred = voting_clf_soft.predict(test)
-
-submission = pd.DataFrame({
-        "PassengerId": X_test["PassengerId"],
-        "Survived": Y_pred
-    })
-
-submission.to_csv('../Titanic-Machine-Learning-from-Disaster/submission.csv', index=False)
-
-
-
+sub(voting_clf_soft)
